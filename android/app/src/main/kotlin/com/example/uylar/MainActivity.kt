a3d771597213add5370c911ed0ef4f1f990cb29f
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-01-10 21:45:40 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/uylar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uylar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Uylar",
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "Uy sotish va sotib olish uchun app",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
